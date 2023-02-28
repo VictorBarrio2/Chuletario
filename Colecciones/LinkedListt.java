8db12fd9b9370cd7b92120351215bf8b18d5388e
AlbertoGM4 <119498958+AlbertoGM4@users.noreply.github.com>
2023-02-28 14:08:02 +0100

    Add files via upload

---
+++ Colecciones/LinkedListt.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colecciones;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Alberto
+ */
+public class LinkedListt {
+    /*
+    En general, las LinkedList pueden ser una buena opción cuando se necesita una estructura 
+    de datos que sea eficiente en la inserción y eliminación de elementos en cualquier posición 
+    de la lista, pero menos eficiente en el acceso aleatorio a los elementos.
+    
+    // Crear una LinkedList
+    LinkedList<String> lista = new LinkedList<>();
+    
+    // Agregar elementos a la lista
+    lista.add("Manzana");
+    lista.add("Naranja");
+    lista.add("Plátano");
+
+    // Obtener el primer y último elemento de la lista
+    String primero = lista.getFirst();
+    String ultimo = lista.getLast();
+
+    // Insertar un elemento en una posición específica
+    lista.add (1, "Pera");
+
+    // Eliminar un elemento de la lista
+    lista.remove ("Naranja");
+
+    // Obtener el número de elementos en la lista
+    int tamaño = lista.size();
+
+    // Recorrer la lista e imprimir cada elemento
+    for (String elemento : lista) {
+    System.out.println(elemento);
+    }
+    */
+
+}

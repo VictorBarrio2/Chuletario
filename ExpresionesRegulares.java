1d88f88db3d731529faccfb918cff0ebb132b716
AlbertoGM4 <119498958+AlbertoGM4@users.noreply.github.com>
2023-03-02 13:12:08 +0100

    Add files via upload

---
+++ ExpresionesRegulares.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chuleta;
+
+/**
+ *
+ * @author Alberto
+ */
+public class ExpresionesRegulares {
+    /*
+    //LA CADENA CONTIENE UN PATRON YA ESPECIFICADO.
+    import java.util.regex.Pattern;
+    import java.util.regex.Matcher;
+
+    public class RegexExample {
+        public static void main(String[] args) {
+            String cadena = "Hola mundo!";
+            String patron = "mundo";
+    
+            Pattern p = Pattern.compile(patron);
+            Matcher m = p.matcher(cadena);
+    
+            if (m.find()) {
+                System.out.println("La cadena contiene el patrón especificado");
+            } else {
+              System.out.println("La cadena no contiene el patrón especificado");
+            }
+        }
+    }
+    
+    //BUSCAR TODAS LA OCURRENCIAS DE UN PATRON EN UNA CADENA DE TEXTO
+    
+    public static void main(String[] args) {
+        String cadena = "La vida es bella, la vida es corta";
+        String patron = "vida";
+    
+        Pattern p = Pattern.compile(patron);
+        Matcher m = p.matcher(cadena);
+    
+        while (m.find()) {
+        System.out.println("Se encontró una ocurrencia en la posición " + m.start() + " - " + m.end());
+        }
+    }
+    
+    //BUSQUEDA DE PATRONES MAS COMPLEJOS
+    
+    public static void main(String[] args) {
+        String cadena = "La fecha de hoy es 02/03/2023";
+        String patron = "\\d{2}/\\d{2}/\\d{4}";
+    
+        Pattern p = Pattern.compile(patron);
+        Matcher m = p.matcher(cadena);
+    
+        if (m.find()) {
+            System.out.println("La fecha encontrada es " + m.group());
+        } else {
+            System.out.println("No se encontró ninguna fecha en la cadena");
+        }
+    }
+    PDF CON MAS CARACTERES.
+    */
+
+}
